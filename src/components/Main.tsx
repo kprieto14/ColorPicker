a178cb14ca321204aa0750d4662082a7303d7615
Kristy Prieto <kprieto14@gmail.com>
2023-09-09 19:48:59 -0400

    Add ability to see/copy hexcode and randomize color

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -17,6 +17,34 @@ export function ColorMain() {
         }
     }
 
+    function handleButton() {
+        setHueValue(Math.floor(Math.random() * 360))
+        setLightValue(Math.floor(Math.random() * 50) + 35)
+    }
+
+    function hslToHex(hue: number, sat: number, light: number):string {
+        light /= 100;
+        const a = sat * Math.min(light, 1 - light) / 100;
+        const f = (n: number) => {
+          const k = (n + hue / 30) % 12;
+          const color = light - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+          return Math.round(255 * color).toString(16).padStart(2, '0');
+        };
+        return `#${f(0)}${f(8)}${f(4)}`;
+    }
+
+    function copyCode() {
+        const text = document.getElementById("hexcode");
+
+        if(text instanceof HTMLInputElement) {
+            // Copy the text inside the text field
+            navigator.clipboard.writeText(text.value);
+            
+            // Alert the copied text
+            alert("Copied the text: " + text.value);
+        }
+    }
+
     const newBackgroundColor = { "--change-color": `hsl(${hueValue},${saturationValue}%, ${lightValue}%` } as React.CSSProperties;
 
     return (
@@ -36,25 +64,25 @@ export function ColorMain() {
                     <article>
                         <label htmlFor="hue">H</label>
                         <input type="range" className="hue" min="0" max="360" value={hueValue} onChange={handleSlider}/>
-                        <input type="text" className="textbox" value={hueValue}/>
+                        <input type="text" className="textbox" value={hueValue} readOnly/>
                     </article>
 
                     <article>
                         <label htmlFor="saturation">S</label>
                         <input type="range" className="saturation" min="0" max="100" value={saturationValue} onChange={handleSlider}/>
-                        <input type="text" className="textbox" value={saturationValue}/>
+                        <input type="text" className="textbox" value={saturationValue} readOnly/>
                     </article>
 
                     <article>
                         <label htmlFor="light">L</label>
                         <input type="range" className="light" min="0" max="100" value={lightValue} onChange={handleSlider}/>
-                        <input type="text" className="textbox" value={lightValue}/>
+                        <input type="text" className="textbox" value={lightValue} readOnly/>
                     </article>
                 </section>
 
                 <footer>
-                    <input type="text" id='hexcode'/>
-                    <button>Randomize Color</button>
+                    <input type="text" id='hexcode' value={hslToHex(hueValue, saturationValue, lightValue)} onClick={copyCode} readOnly/>
+                    <button onClick={handleButton}>Randomize Color</button>
                 </footer>
             </main>
         </div>
